f230ff36062dc7d5b3bac42252c5c9adb92e40ec
Syzhaa <saleh.slh02@gmail.com>
2026-09-09 17:17:40 +0800

    perf(icons): bundle material-symbols locally to prevent font ligature loading delays (FOIT)

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "material-symbols/outlined.css";
 import "./index.css";
 import App from "./App.jsx";
 import { ErrorBoundary } from "./components/ErrorBoundary.jsx";
