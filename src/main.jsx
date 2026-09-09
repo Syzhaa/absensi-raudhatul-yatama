2d5a9574912a9c05f0a67c31cfaf62ba693e6aed
Syzhaa <saleh.slh02@gmail.com>
2026-09-09 15:46:31 +0800

    refactor: self-host Font Awesome via npm (remove cdnjs dependency)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import App from "./App.jsx";
 import { ErrorBoundary } from "./components/ErrorBoundary.jsx";
